876f825669d020cb756f9e49580fbb8e0f62c739
wyx <1105017602@qq.com>
2023-05-03 18:44:14 +0800

    取消周天判断，每天都打卡

--- main.py
+++ main.py
@@ -241,12 +241,12 @@ if __name__ == "__main__":
     uses = Do()
     for i in user_list:
         print(f"---------用户：{encryption(i)} 开始打卡------------")
-        weekday = datetime.datetime.now().weekday() + 1
-        print(f"今天是星期 {weekday}，{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
+        # weekday = datetime.datetime.now().weekday() + 1
+        print(f"现在是{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
         uses.get_JWSESSION(i, password)
         # 星期天才打卡，其他时间更新JWSESSION
-        if weekday == 7:
-            uses.punch()
+        # if weekday == 7:
+        uses.punch()
 
         time.sleep(1)
     print("---------------------end-------------------------")
