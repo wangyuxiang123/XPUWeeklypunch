5f3f5c9ea8d46a4b89cb473ca80f26d45d8caecc
wangyuxiang111 <1105017602@qq.com>
2024-11-21 12:58:33 +0800

    调试

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ def get_list(h):
     url = "https://gw.wozaixiaoyuan.com/sign/mobile/receive/getMySignLogs?page=1&size=1"
     res = requests.get(url, headers=h)
     print(res)
-    print(res.text)
+    print(res.text.encode("utf8"))
     res_text = res.json()["data"][0]
     print("标题：", res_text["signContext"])
 
